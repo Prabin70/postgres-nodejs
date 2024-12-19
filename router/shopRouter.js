a6da90aa1be08aaec4c787f70dafc1ee90fbdabe
Prabin70 <xthaprabin125@gmail.com>
2024-12-20 00:21:28 +0545

    postgreSQLwithNodejs

--- router/shopRouter.js
+++ router/shopRouter.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../database/database')
+
+const Shop = require('../model/Shop')
 router.get('/', (req, res, next) => {
-    res.send('Hello from the API');
+    Shop.findAll()
+        .then(shops => {
+            console.log(shops);
+            res.sendStatus(200);
+
+        })
+        .catch(err => {
+            console.error(err);
+            res.sendStatus(500);
+        });
 
 });
 
